10b7f1931197d88973164f58c88129b5cbc2a01b
2tefan <2tefan@tuta.io>
2022-02-09 16:37:41 +0100

    Formatting

--- src/components/TrafficMap.vue
+++ src/components/TrafficMap.vue
@@ -1,128 +1,127 @@
 <template>
-  <div style="height: 93vh">
-    <l-map
-      v-model="zoom"
-      v-model:zoom="zoom"
-      :center="center"
-      :minZoom="minZoom"
-      :maxZoom="maxZoom"
-      :maxBounds="maxBounds"
-      :zoomAnimation="true"
-      :fadeAnimation="true"
-    >
-      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <l-geo-json :geojson="geojson" :options="options" />
+<div style="height: 93vh">
+    <l-map v-model="zoom" v-model:zoom="zoom" :center="center" :minZoom="minZoom" :maxZoom="maxZoom" :maxBounds="maxBounds" :zoomAnimation="true" :fadeAnimation="true">
+        <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+        <l-geo-json :geojson="geojson" :options="options" />
     </l-map>
-  </div>
+</div>
 </template>
+
 <script>
-import { LMap, LTileLayer, LGeoJson } from "@vue-leaflet/vue-leaflet";
+import {
+    LMap,
+    LTileLayer,
+    LGeoJson
+} from "@vue-leaflet/vue-leaflet";
 //import NodeInfo from "./components/NodeInfo.vue";
 import "leaflet/dist/leaflet.css";
-import { RepoFactory } from "./../repos/RepoFactory";
+import {
+    RepoFactory
+} from "./../repos/RepoFactory";
 const GeoJsonRepo = RepoFactory.get("nodes");
 const NodeInfoRepo = RepoFactory.get("nodeInfo");
 export default {
-  name: "TrafficMap",
-  components: {
-    LMap,
-    LTileLayer,
-    LGeoJson,
-    // NodeInfo,
-  },
-  data() {
-    return {
-      center: [46.6228162, 14.3079604],
-      zoom: 16,
-      maxZoom: 20,
-      minZoom: 13,
-      maxBounds: [
-        [46.6613, 14.2347],
-        [46.5714, 14.405],
-      ],
-      //url: "http://tiles.lumi200.at/hot/{z}/{x}/{y}.png",
-      url: "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="http://commean.eu">Commean</a>',
-      geojson: null,
-      options: {
-        onEachFeature: this.onEachFeature,
-      },
-    };
-  },
-  beforeMount() {
-    this.fetchGeoJson();
-    this.setup();
-  },
-  methods: {
-    async getData(id) {
-      return await NodeInfoRepo.getNodeInfo(id);
+    name: "TrafficMap",
+    components: {
+        LMap,
+        LTileLayer,
+        LGeoJson,
+        // NodeInfo,
     },
-    async fetchGeoJson() {
-      try {
-        const { data } = await GeoJsonRepo.get();
-        this.geojson = data;
-      } catch (error) {
-        if (error.response) {
-          this.$swal.fire({
-            icon: "error",
-            title: "API error",
-            text: error.response.data.message,
-          });
-        } else {
-          this.$swal.fire({
-            icon: "error",
-            title: "No response from Server",
-            text: "The server could not be reached",
-          });
-        }
-      }
+    data() {
+        return {
+            center: [46.6228162, 14.3079604],
+            zoom: 16,
+            maxZoom: 20,
+            minZoom: 13,
+            maxBounds: [
+                [46.6613, 14.2347],
+                [46.5714, 14.405],
+            ],
+            //url: "http://tiles.lumi200.at/hot/{z}/{x}/{y}.png",
+            url: "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | <a href="http://commean.eu">Commean</a>',
+            geojson: null,
+            options: {
+                onEachFeature: this.onEachFeature,
+            },
+        };
     },
-
-    setup() {
-      document.body.style.overflow = "hidden";
-      document.oncontextmenu = function () {
-        return false;
-      };
+    beforeMount() {
+        this.fetchGeoJson();
+        this.setup();
     },
-    onEachFeature(feature, layer) {
-      layer.bindPopup(`Waiting for data from Node:\n${feature.id}`, {
-        maxWidth: "400",
-        width: "200",
-        className: "custom-popup",
-      });
-      layer.on("click", async function () {
-        const { data } = await NodeInfoRepo.getNodeInfo(feature.id);
-        console.log(data);
+    methods: {
+        async getData(id) {
+            return await NodeInfoRepo.getNodeInfo(id);
+        },
+        async fetchGeoJson() {
+            try {
+                const {
+                    data
+                } = await GeoJsonRepo.get();
+                this.geojson = data;
+            } catch (error) {
+                if (error.response) {
+                    this.$swal.fire({
+                        icon: "error",
+                        title: "API error",
+                        text: error.response.data.message,
+                    });
+                } else {
+                    this.$swal.fire({
+                        icon: "error",
+                        title: "No response from Server",
+                        text: "The server could not be reached",
+                    });
+                }
+            }
+        },
 
-        layer.bindPopup(
-          `<div> 
-              <h3>${data.tcnId}</h3>
-              <table>
-                <tr>
-                  <th>Current</th>
-                  <td>${data.trafficSituation}</td>
-                </tr>
-                <tr>
-                  <th>Time loss</th>
-                  <td>${data.averageTimeInPicture}</td>
-                </tr>
-              </table>
-            </div>`,
-          {
-            maxWidth: "400",
-            width: "200",
-            className: "custom-popup",
-          }
-        );
+        setup() {
+            document.oncontextmenu = function () {
+                return false;
+            };
+        },
+        onEachFeature(feature, layer) {
+            layer.bindPopup(`Waiting for data from Node:\n${feature.id}`, {
+                maxWidth: "400",
+                width: "200",
+                className: "custom-popup",
+            });
+            layer.on("click", async function () {
+                const {
+                    data
+                } = await NodeInfoRepo.getNodeInfo(feature.id);
+                console.log(data);
 
-        layer.closePopup();
-        layer.openPopup();
+                layer.bindPopup(
+                    `<div>
+                      <h3>${data.tcnId}</h3>
+                      <table>
+                        <tr>
+                          <th>Current</th>
+                          <td>${data.trafficSituation}</td>
+                        </tr>
+                        <tr>
+                          <th>Time loss</th>
+                          <td>${data.averageTimeInPicture}</td>
+                        </tr>
+                      </table>
+                    </div>`, {
+                        maxWidth: "400",
+                        width: "200",
+                        className: "custom-popup",
+                    }
+                );
 
-        //console.log(data.tcnId);
-        //console.log(getData(feature.id));
-      });
+                layer.closePopup();
+                layer.openPopup();
+
+                //console.log(data.tcnId);
+                //console.log(getData(feature.id));
+            });
+        },
     },
-  },
 };
 </script>
