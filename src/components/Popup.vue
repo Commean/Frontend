f6f3bf970e558586c89d83b2e160ea9e47d4233f
2tefan <2tefan@tuta.io>
2022-02-11 10:20:55 +0100

    Update design

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,16 +1,12 @@
 <template>
-<div>
+<div class="popup">
     <h1>{{data.tcnId}}</h1>
-    <table>
-        <tr>
-            <th>Current</th>
-            <td>{{data.trafficSituation}}</td>
-        </tr>
-        <tr>
-            <th>Time loss</th>
-            <td>{{data.averageTimeInPicture}}</td>
-        </tr>
-    </table>
+    <div class="overview">
+        <h2 id="current_title">Current</h2>
+        <img id="current" :src="require('/src/assets/popup/currentSituation.svg')" alt="">
+        <h2 id="time_loss_title">Time loss</h2>
+        <p id="time_loss">{{data.averageTimeInPicture}} min</p>
+    </div>
 </div>
 </template>
 
@@ -22,7 +18,54 @@ export default {
 </script>
 
 <style lang="scss">
-h1 {
-    font-size: 1.7em;
+.popup {
+    display: grid;
+    grid-template-areas:
+        'title title'
+        'table table'
+        'status .';
+    //grid-template-columns: auto 200px;
+    //grid-template-rows: 80px auto;
+
+    h1 {
+        font-size: 1.7em;
+        grid-area: title;
+    }
+
+    .overview {
+        display: inline-grid;
+        grid-area: table;
+
+        grid-template-areas:
+            'current_title current'
+            'time_loss_title time_loss';
+        grid-template-columns: auto 150px;
+
+        h2 {
+            font-size: 1.2em;
+        }
+
+        h2,
+        p {
+            padding: 0;
+            margin: auto 0;
+        }
+
+        #current_title {
+            grid-area: current_title;
+        }
+
+        #current {
+            grid-area: current;
+        }
+
+        #time_loss_title {
+            grid-area: time_loss_title;
+        }
+
+        #time_loss {
+            grid-area: time_loss;
+        }
+    }
 }
 </style>
